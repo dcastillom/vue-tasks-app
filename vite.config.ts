13d52238bf64cb205352c0a812a7fd707285324a
Daniel Castillo <d.castillo.marfull@accenture.com>
2024-01-08 10:30:16 +0100

    chore: adds aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+const alias = {
+  '@': fileURLToPath(new URL('./src', import.meta.url)),
+  '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+  '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+}
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias,
+  },
 })
